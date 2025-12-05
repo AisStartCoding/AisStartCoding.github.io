f6cbc0d9efe476b6d5391bc4a66c486d1100393a
AisStartCoding <165084957+AisStartCoding@users.noreply.github.com>
2025-12-06 04:23:40 +0600

    Updates

--- _next/static/ZrGefemDJoD1aji4S2Jsm/_buildManifest.js
+++ _next/static/ZrGefemDJoD1aji4S2Jsm/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/portfolio-website//_next/:path+",
-        "destination": "/portfolio-website/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
